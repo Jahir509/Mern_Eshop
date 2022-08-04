29896956985b7c153a968fabdc01eb0abf80bf08
jahir509 <me.jahirahmed@gmail.com>
2022-08-04 18:31:10 +0600

    CORS

--- back-end/app.js
+++ back-end/app.js
@@ -16,21 +16,8 @@ const errorHandler = require('./helpers/error-handler');
 
 
 // // Middleware
-// app.use(cors);
+app.use(cors());
 // app.options('*',cors());
-
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-	  "Access-Control-Allow-Headers",
-	  "Origin,X-Requested-With,Content-Type,Accept"
-	);
-	res.setHeader(
-	  "Access-Control-Allow-Methods",
-	  "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-	);
-	next();
-  });
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
