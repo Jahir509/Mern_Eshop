c93c8f7430de2765e373129550d7931823bea538
jahir509 <me.jahirahmed@gmail.com>
2022-08-22 08:55:44 +0600

    presigned url fetch

--- back-end/routers/upload.js
+++ back-end/routers/upload.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const aws = require('aws-sdk');
+const uuid = require('uuid').v1;
 
 const s3 = new aws.S3({
     accessKeyId: process.env.S3_ACCESS_KEY,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 })
 
-router.get('/presign',async (req,res)=>{
-    console.log(req.user);
+router.get('/presign', (req,res)=>{
+    // console.log(req.user);
+    const key = `users/${uuid()}.jpeg`
     s3.getSignedUrl('putObject',
         {
             Bucket: 'ishop-bucket',
-            ContentType:'jpeg',
-            // Key:
+            ContentType: 'jpeg',
+            Key: key
+        },(err,url)=>{
+            res.send(url)
         }
     )
 })
